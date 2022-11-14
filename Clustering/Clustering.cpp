0227567010e54bd341ce643fccd5216f626fec22
Shivam Mahajan <78159964+Cursed-Ninja@users.noreply.github.com>
2022-11-14 21:57:56 +0530

    Create Clustering.cpp

---
+++ Clustering/Clustering.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    srand(time(0));
+    int l, r;
+    cin >> l >> r;
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rand() % (r - l + 1) + l;
+    }
+    int m;
+    cin >> m;
+    vector<vector<double>> v(m);
+    vector<double> g(m), sum(m);
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> g[i];
+    }
+
+    for (int _ = 0; _ < n / 2; _++)
+    {
+        v.clear();
+        sum.clear();
+        v.resize(m);
+        sum.resize(m);
+        for (int i = 0; i < m; i++)
+        {
+            v[i].push_back(g[i]);
+            sum[i] = g[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            int closest = 0;
+            double value = INT_MAX;
+            for (int j = 0; j < m; j++)
+            {
+                if ((sum[j] + arr[i]) / (v[j].size() + 1) < value)
+                {
+                    value = (sum[j] + arr[i]) / (v[j].size() + 1);
+                    closest = j;
+                }
+            }
+
+            sum[closest] += arr[i];
+            v[closest].push_back(arr[i]);
+            v[closest][0] = sum[closest] / v[closest].size();
+        }
+
+        bool flag = true;
+        for (int j = 0; j < m; j++)
+        {
+            if (abs(g[j] - v[j][0]) > 0.0001)
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        if (flag)
+            break;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+            cout << v[i][j] << " ";
+        cout << endl
+             << endl;
+    }
+    cout << endl;
+}
